6a5d8ba8b69fe7e36d689d7ec106b666a34d453e
NymouseX <saibaougokasu@gmail.com>
2021-11-03 13:20:07 +0700

    TermuxError

---
+++ Termux.sh
@@ -0,0 +1,468 @@
+clear
+echo "
+███████╗██████╗░██████╗░░█████╗░██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+█████╗░░██████╔╝██████╔╝██║░░██║██████╔╝
+██╔══╝░░██╔══██╗██╔══██╗██║░░██║██╔══██╗
+███████╗██║░░██║██║░░██║╚█████╔╝██║░░██║
+╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
+
+████████╗███████╗██████╗░███╗░░░███╗██╗░░░██╗██╗░░██╗
+╚══██╔══╝██╔════╝██╔══██╗████╗░████║██║░░░██║╚██╗██╔╝
+░░░██║░░░█████╗░░██████╔╝██╔████╔██║██║░░░██║░╚███╔╝░
+░░░██║░░░██╔══╝░░██╔══██╗██║╚██╔╝██║██║░░░██║░██╔██╗░
+░░░██║░░░███████╗██║░░██║██║░╚═╝░██║╚██████╔╝██╔╝╚██╗
+░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░╚═════╝░╚═╝░░╚═╝" | lolcat
+echo "***********************NYX*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************NYX*********************************"|lolcat
+sleep 1
+echo "nama kamu :"
+read -p " |NYX}--> " NM
+clear
+sleep 2
+toilet -f big -F gay $NM
+sleep 1
+echo "***********************$NM*********************************"|lolcat
+echo "Author By   : Nymousex"|lolcat
+echo "My Team     : Cyber Hanter Exploit"|lolcat
+echo "My Youtube  : Nymousex" | lolcat
+echo "***********************$NM*********************************"|lolcat
+sleep 1
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+pkg remove -y game-repo
+pkg remove -y science-repo
+pkg update
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>                              <]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>                            <<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>                          <<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>                        <<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>                      <<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>                    <<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>                  <<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>                <<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>              <<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>            <<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>          <<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>        <<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>      <<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>    <<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Peroses Perbaikan Termux Sedang Berjalan""[>>>>>>>>>>>>>>>^^<<<<<<<<<<<<<<<]"
+sleep 0.1
+clear
+echo "Selamat Termux Kamu Sudah Bisa Menjalankan MISI" | lolcat
+echo 
+echo "Apakah Kamu Ingin Memulai lagi?"
+echo "y"
+echo "n"
+read -p "|$NM}-->" JWB
+
+if [ $JWB = y ] || [ $JWB = Y ]
+then
+bash Termux.sh
+fi
+
+if [ $JWB = n ] || [ $JWB = N ]
+then
+echo "the and"
+sleep 0.1
+clear 
+echo "The and ."
+sleep 0.1
+clear 
+echo "tHe and .."
+sleep 0.1
+clear 
+echo "thE and ..."
+sleep 0.1
+clear 
+echo "the And ."
+sleep 0.1
+clear 
+echo "the aNd .."
+sleep 0.1
+clear 
+echo "the anD ..." 
+sleep 0.1
+clear 
+echo "The and ."
+sleep 0.1
+clear 
+echo "tHe and .."
+sleep 0.1
+clear 
+echo "thE and ..."
+sleep 0.1
+clear
+echo "the And ."
+sleep 0.1
+clear 
+echo "the aNd .."
+sleep 0.1
+clear 
+echo "the anD ..." 
+sleep
+clear
+echo "the and"
+sleep 0.1
+clear 
+echo "The and ."
+sleep 0.1
+clear 
+echo "tHe and .."
+sleep 0.1
+clear 
+echo "thE and ..."
+sleep 0.1
+clear 
+echo "the And ."
+sleep 0.1
+clear 
+echo "the aNd .."
+sleep 0.1
+clear 
+echo "the anD ..." 
+sleep 0.1
+clear 
+echo "The and ."
+sleep 0.1
+clear 
+echo "tHe and .."
+sleep 0.1
+clear 
+echo "thE and ..."
+sleep 0.1
+clear
+echo "the And ."
+sleep 0.1
+clear 
+echo "the aNd .."
+sleep 0.1
+clear 
+echo "the anD ..." 
+sleep
+clear
+echo "the and"
+sleep 0.1
+clear 
+echo "The and ."
+sleep 0.1
+clear 
+echo "tHe and .."
+sleep 0.1
+clear 
+echo "thE and ..."
+sleep 0.1
+clear 
+echo "the And ."
+sleep 0.1
+clear 
+echo "the aNd .."
+sleep 0.1
+clear 
+echo "the anD ..." 
+sleep 0.1
+clear 
+echo "The and ."
+sleep 0.1
+clear 
+echo "tHe and .."
+sleep 0.1
+clear 
+echo "thE and ..."
+sleep 0.1
+clear
+echo "the And ."
+sleep 0.1
+clear 
+echo "the aNd .."
+sleep 0.1
+clear 
+echo "the anD ..." 
+sleep
+clear
+echo "The and ."
+sleep 0.1
+clear 
+echo "tHe and .."
+sleep 0.1
+clear 
+echo "thE and ..."
+sleep 0.1
+clear
+echo "THE AND ..."
+sleep 0.1
+clear 
+echo "Bye Bye" | lolcat
+sleep 2
+exit
+fi
+
+
+
+
+
+
+
+
+
